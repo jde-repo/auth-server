d9b2b7527a4b469f39ef232d08712d3e3da390bb
1101683 <daeul.jeong@11stcorp.com>
2025-10-04 00:31:31 +0900

    fix: API Docs 404 오류 해결

--- src/main/java/com/daeul/auth/config/SecurityConfig.java
+++ src/main/java/com/daeul/auth/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/auth/logout",
                         "/swagger-ui.html",
                         "/swagger-ui/**",
+                        "/v3/api-docs",
                         "/v3/api-docs/**",
                         "/swagger-resources/**",
                         "/webjars/**"
